3f0caef2513a9f82179e004a5a797aab2eb169e3
cfoster5 <cfoster3204@gmail.com>
2021-02-14 19:19:52 -0600

    Added declaration file for CategoryControl component to eliminate TypeScript warnings

---
+++ src/components/CategoryControl.d.ts
@@ -0,0 +1,16 @@
+// This file exists for two purposes:
+// 1. Ensure that both ios and android files present identical types to importers.
+// 2. Allow consumers to import the module as if typescript understood react-native suffixes.
+import CategoryControl from './CategoryControl.ios';
+import * as ios from './CategoryControl.ios';
+import CategoryControlAndroid from './CategoryControl.android';
+import * as android from './CategoryControl.android';
+
+declare var _test: typeof ios;
+declare var _test: typeof android;
+
+declare var _testDefault: typeof CategoryControl;
+declare var _testDefault: typeof CategoryControlAndroid;
+
+export * from './CategoryControl.ios';
+export default CategoryControl;

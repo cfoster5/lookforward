d677f57943fce55c08f67e16d757e5176a4b25d9
cfoster5 <cfoster3204@gmail.com>
2021-02-28 09:21:56 -0600

    Update stack screens to update when countdown games and movies are updated

--- src/navigation/CountdownStack.tsx
+++ src/navigation/CountdownStack.tsx
@@ -4,6 +4,7 @@ import Countdown from '../screens/Countdown';
 import { CompositeNavigationProp, RouteProp } from '@react-navigation/native';
 import { Navigation } from '../../types';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import Details from '../screens/Details';
 
 type CountdownStackNavProp = CompositeNavigationProp<
   StackNavigationProp<Navigation.CountdownStackParamList, "Countdown">,
@@ -12,21 +13,21 @@ type CountdownStackNavProp = CompositeNavigationProp<
 
 interface Props {
   navigation: CountdownStackNavProp,
-  route: RouteProp<Navigation.TabNavigationParamList, "Countdown">
+  route: RouteProp<Navigation.TabNavigationParamList, "Countdown">,
+  countdownMovies: any[],
+  countdownGames: any[]
 }
 
 const Stack = createStackNavigator<any>();
-export function CountdownStack({ navigation, route }: Props) {
+export function CountdownStack({ navigation, route, countdownMovies, countdownGames }: Props) {
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Countdown"
-        component={Countdown}
-        initialParams={{
-          // user: route.params.user
-          uid: route.params.uid
-        }}
-      />
+      <Stack.Screen name="Countdown" initialParams={{ uid: route.params.uid }}>
+        {props => <Countdown {...props} countdownMovies={countdownMovies} countdownGames={countdownGames} />}
+      </Stack.Screen>
+      <Stack.Screen name="Details" initialParams={{ uid: route.params.uid, igdbCreds: route.params.igdbCreds }}>
+        {props => <Details {...props} countdownMovies={countdownMovies} countdownGames={countdownGames} />}
+      </Stack.Screen>
     </Stack.Navigator>
   )
 }

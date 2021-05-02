9ce084b7a057241f2412efe4a87cf8ee752c4bd3
cfoster5 <cfoster3204@gmail.com>
2021-05-02 09:47:27 -0500

    Update Trailer.tsx

--- src/components/Trailer.tsx
+++ src/components/Trailer.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Appearance, Dimensions, Image, Linking, Pressable, Text, View } from "react-native";
+import { ColorSchemeName, Dimensions, Image, Linking, Pressable, Text, View } from "react-native";
 import { iOSUIKit } from "react-native-typography";
 import { IGDB, TMDB } from "../../types";
 
-function Trailer({ video, index }: { video: TMDB.Movie.VideoResult | IGDB.Game.Video, index: number }) {
-  // const colorScheme = Appearance.getColorScheme();
-  const colorScheme = "dark"
+function Trailer({ video, index, colorScheme }: { video: TMDB.Movie.VideoResult | IGDB.Game.Video, index: number, colorScheme: ColorSchemeName }) {
   return (
     // <Pressable key={index} onPress={() => Linking.openURL(`https://www.youtube.com/watch?v=${video.key}`)}>
     <Pressable key={index} onPress={() => Linking.openURL((video as TMDB.Movie.VideoResult).key ? `https://www.youtube.com/watch?v=${(video as TMDB.Movie.VideoResult).key}`: `https://www.youtube.com/watch?v=${(video as IGDB.Game.Video).video_id}`)}>

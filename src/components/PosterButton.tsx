6c45510c916f4a28b7eca82f1ed922e0143cf197
cfoster5 <cfoster3204@gmail.com>
2021-05-02 09:46:37 -0500

    Removed conditional rendering inside of PosterButton as component is now only being rendered within the MoviePoster component that sits a level above

--- src/components/PosterButton.tsx
+++ src/components/PosterButton.tsx
@@ -6,13 +6,12 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import firestore from '@react-native-firebase/firestore';
 
 interface Props {
-  mediaType: "game" | "movie",
   data: TMDB.Movie.Movie | IGDB.Game.Game
   inCountdown: boolean;
   uid: string;
 }
 
-function PosterButton({ mediaType, data, inCountdown, uid }: Props) {
+function PosterButton({ data, inCountdown, uid }: Props) {
 
   async function addToList() {
     try {
@@ -39,32 +38,28 @@ function PosterButton({ mediaType, data, inCountdown, uid }: Props) {
   }
 
   return (
-    <>
-      {mediaType === "movie" &&
-        <Pressable onPress={() => inCountdown ? deleteItem() : addToList()} style={{ position: "absolute", zIndex: 1, bottom: 4, right: 4 }}>
-          <View style={{
-            height: 36,
-            width: 36,
-            borderRadius: 18,
-            borderWidth: 1,
-            borderColor: "#222",
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-            <View style={{
-              height: 36,
-              width: 36,
-              borderRadius: 18,
-              backgroundColor: "#333",
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-              <Ionicons name={inCountdown ? "checkmark-outline" : "add-outline"} color={iOSColors.white} size={28} style={{ textAlign: "center" }} />
-            </View>
-          </View>
-        </Pressable>
-      }
-    </>
+    <Pressable onPress={() => inCountdown ? deleteItem() : addToList()} style={{ position: "absolute", zIndex: 1, bottom: 4, right: 4 }}>
+      <View style={{
+        height: 36,
+        width: 36,
+        borderRadius: 18,
+        borderWidth: 1,
+        borderColor: "#222",
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <View style={{
+          height: 36,
+          width: 36,
+          borderRadius: 18,
+          backgroundColor: "#333",
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+          <Ionicons name={inCountdown ? "checkmark-outline" : "add-outline"} color={iOSColors.white} size={28} style={{ textAlign: "center" }} />
+        </View>
+      </View>
+    </Pressable>
   )
 }
 

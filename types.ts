60124fb88ca0263e2eaa85997e126561c6778dde
cfoster5 <cfoster3204@gmail.com>
2021-05-08 10:43:39 -0500

    Update types.ts

--- types.ts
+++ types.ts
@@ -101,28 +101,30 @@ export namespace IGDB {
 
 export namespace TMDB {
 
+  interface Genre {
+    id: number;
+    name: string;
+  }
+
+  interface ProductionCompany {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }
+
+  interface ProductionCountry {
+    iso_3166_1: string;
+    name: string;
+  }
+
+  interface SpokenLanguage {
+    english_name?: string;
+    iso_639_1: string;
+    name: string;
+  }
+
   export namespace Movie {
-    export interface Genre {
-      id: number;
-      name: string;
-    }
-
-    export interface ProductionCompany {
-      id: number;
-      logo_path: string;
-      name: string;
-      origin_country: string;
-    }
-
-    export interface ProductionCountry {
-      iso_3166_1: string;
-      name: string;
-    }
-
-    export interface SpokenLanguage {
-      iso_639_1: string;
-      name: string;
-    }
 
     export interface Cast {
       cast_id: number;
@@ -266,12 +268,102 @@ export namespace TMDB {
     }
   }
 
+  export namespace Show {
+
+    interface CreatedBy {
+      id: number;
+      credit_id: string;
+      name: string;
+      gender: number;
+      profile_path: string;
+    }
+
+    interface LastEpisodeToAir {
+      air_date: string;
+      episode_number: number;
+      id: number;
+      name: string;
+      overview: string;
+      production_code: string;
+      season_number: number;
+      still_path: string;
+      vote_average: number;
+      vote_count: number;
+    }
+
+    interface NextEpisodeToAir {
+      air_date: string;
+      episode_number: number;
+      id: number;
+      name: string;
+      overview: string;
+      production_code: string;
+      season_number: number;
+      still_path: string;
+      vote_average: number;
+      vote_count: number;
+    }
+
+    interface Network {
+      name: string;
+      id: number;
+      logo_path: string;
+      origin_country: string;
+    }
+
+    interface Season {
+      air_date: string;
+      episode_count: number;
+      id: number;
+      name: string;
+      overview: string;
+      poster_path: string;
+      season_number: number;
+    }
+
+    export interface Show {
+      backdrop_path: string;
+      created_by: CreatedBy[];
+      episode_run_time: number[];
+      first_air_date: string;
+      genres: Genre[];
+      homepage: string;
+      id: number;
+      in_production: boolean;
+      languages: string[];
+      last_air_date: string;
+      last_episode_to_air: LastEpisodeToAir;
+      name: string;
+      next_episode_to_air: NextEpisodeToAir;
+      networks: Network[];
+      number_of_episodes: number;
+      number_of_seasons: number;
+      origin_country: string[];
+      original_language: string;
+      original_name: string;
+      overview: string;
+      popularity: number;
+      poster_path: string;
+      production_companies: ProductionCompany[];
+      production_countries: ProductionCountry[];
+      seasons: Season[];
+      spoken_languages: SpokenLanguage[];
+      status: string;
+      tagline: string;
+      type: string;
+      vote_average: number;
+      vote_count: number;
+    }
+
+  }
+
 }
 
 export namespace Trakt {
 
-  export interface Ids {
+  interface Ids {
     trakt: number;
+    slug?: string;
     tvdb?: number;
     imdb: string;
     tmdb?: number;
@@ -285,7 +377,7 @@ export namespace Trakt {
     ids: Ids;
   }
 
-  export interface Ids2 {
+  interface Ids2 {
     trakt: number;
     slug: string;
     tvdb?: number;
@@ -294,125 +386,67 @@ export namespace Trakt {
     tvrage?: number;
   }
 
-  export interface CreatedBy {
-    id: number;
-    credit_id: string;
-    name: string;
-    gender: number;
-    profile_path: string;
-  }
-
-  export interface Genre {
-    id: number;
-    name: string;
-  }
-
-  export interface LastEpisodeToAir {
-    air_date: string;
-    episode_number: number;
-    id: number;
-    name: string;
-    overview: string;
-    production_code: string;
-    season_number: number;
-    still_path: string;
-    vote_average: number;
-    vote_count: number;
-  }
-
-  export interface NextEpisodeToAir {
-    air_date: string;
-    episode_number: number;
-    id: number;
-    name: string;
-    overview: string;
-    production_code: string;
-    season_number: number;
-    still_path: string;
-    vote_average: number;
-    vote_count: number;
-  }
-
-  export interface Network {
-    name: string;
-    id: number;
-    logo_path: string;
-    origin_country: string;
-  }
-
-  export interface ProductionCompany {
-    id: number;
-    logo_path: string;
-    name: string;
-    origin_country: string;
-  }
-
-  export interface ProductionCountry {
-    iso_3166_1: string;
-    name: string;
-  }
-
-  export interface Season {
-    air_date: string;
-    episode_count: number;
-    id: number;
-    name: string;
-    overview: string;
-    poster_path: string;
-    season_number: number;
-  }
-
-  export interface SpokenLanguage {
-    english_name: string;
-    iso_639_1: string;
-    name: string;
-  }
-
-  export interface TmdbData {
-    backdrop_path: string;
-    created_by: CreatedBy[];
-    episode_run_time: number[];
-    first_air_date: string;
-    genres: Genre[];
-    homepage: string;
-    id: number;
-    in_production: boolean;
-    languages: string[];
-    last_air_date: string;
-    last_episode_to_air: LastEpisodeToAir;
-    name: string;
-    next_episode_to_air: NextEpisodeToAir;
-    networks: Network[];
-    number_of_episodes: number;
-    number_of_seasons: number;
-    origin_country: string[];
-    original_language: string;
-    original_name: string;
-    overview: string;
-    popularity: number;
-    poster_path: string;
-    production_companies: ProductionCompany[];
-    production_countries: ProductionCountry[];
-    seasons: Season[];
-    spoken_languages: SpokenLanguage[];
-    status: string;
-    tagline: string;
-    type: string;
-    vote_average: number;
-    vote_count: number;
+  export interface Airs {
+    day: string;
+    time: string;
+    timezone: string;
   }
 
   export interface Show {
     title: string;
     year: number;
     ids: Ids2;
-    tmdbData: TmdbData;
+    tmdbData?: TMDB.Show.Show;
+    overview: string;
+    first_aired: Date;
+    airs: Airs;
+    runtime: number;
+    certification: string;
+    network: string;
+    country: string;
+    trailer: string;
+    homepage: string;
+    status: string;
+    rating: number;
+    votes: number;
+    comment_count: number;
+    updated_at: Date;
+    language: string;
+    available_translations: string[];
+    genres: string[];
+    aired_episodes: number;
   }
 
   export interface ShowPremiere {
     first_aired: Date;
     episode: Episode;
     show: Show;
+    // documentID is from Firestore
+    documentID?: number;
+  }
+
+  export interface ShowSearch {
+    type: string;
+    score: number;
+    show: Show;
+    // documentID is from Firestore
+    documentID?: number;
+  }
+
+  export interface NextEpisode {
+    season: number;
+    number: number;
+    title: string;
+    ids: Ids;
+    number_abs?: any;
+    overview: string;
+    rating: number;
+    votes: number;
+    comment_count: number;
+    first_aired: Date;
+    updated_at: Date;
+    available_translations: any[];
+    runtime: number;
   }
 
 }

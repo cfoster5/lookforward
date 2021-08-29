f71aad90b77472f3483cb0325130a22980541d2e
cfoster5 <cfoster3204@gmail.com>
2021-08-29 17:59:51 -0500

    Update SearchPerson.tsx

--- src/components/SearchPerson.tsx
+++ src/components/SearchPerson.tsx
@@ -1,12 +1,13 @@
 import { StackNavigationProp } from "@react-navigation/stack";
 import React, { useContext, useEffect } from "react";
-import { Pressable, Text, View } from "react-native";
+import { Dimensions, Pressable, Text, View } from "react-native";
 import { iOSColors, iOSUIKit } from "react-native-typography";
 import { reusableStyles } from "../helpers/styles";
 import ThemeContext from "../contexts/ThemeContext";
 import FastImage from "react-native-fast-image";
 import { Navigation } from "../interfaces/navigation";
 import { TMDB } from "../interfaces/tmdb";
+import { StyleSheet } from "react-native";
 
 interface Props {
   navigation: StackNavigationProp<Navigation.FindStackParamList, "Details">,
@@ -19,13 +20,13 @@ function SearchPerson({ navigation, person }: Props) {
     <Pressable style={{ marginRight: 16 }} onPress={() => navigation.push("Actor", person)}>
       {person.profile_path &&
         <FastImage
-          style={reusableStyles.searchCredit}
+          style={styles.searchCredit}
           source={{ uri: `https://image.tmdb.org/t/p/w300${person.profile_path}` }}
         />
       }
       {!person.profile_path &&
         <View style={{
-          ...reusableStyles.searchCredit,
+          ...styles.searchCredit,
           borderWidth: 1,
           borderColor: colorScheme === "dark" ? "#1f1f1f" : "white",
           flexDirection: 'row',
@@ -37,9 +38,19 @@ function SearchPerson({ navigation, person }: Props) {
           </Text>
         </View>
       }
-      <Text style={colorScheme === "dark" ? { ...reusableStyles.date, maxWidth: reusableStyles.searchCredit.width, textAlign: "center" } : iOSUIKit.body}>{person.name}</Text>
+      <Text style={colorScheme === "dark" ? { ...reusableStyles.date, maxWidth: styles.searchCredit.width, textAlign: "center" } : iOSUIKit.body}>{person.name}</Text>
     </Pressable>
   )
 }
+
+const styles = StyleSheet.create({
+  searchCredit: {
+    // I don't know why 18 works here to center the right-most image but it works on every iOS device tested
+    width: (Dimensions.get("window").width / 3.5) - 18,
+    height: ((Dimensions.get("window").width / 3.5) - 18) * 1.5,
+    borderRadius: 8,
+    marginBottom: 8
+  }
+})
 
 export default SearchPerson;

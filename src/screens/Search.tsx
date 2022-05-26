0480ca759330b924bd75f5ab3584b9533082b022
cfoster5 <cfoster3204@gmail.com>
2022-05-26 11:12:25 -0500

    Update Search.tsx

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -412,9 +412,7 @@ function Search({ navigation, route }: Props) {
               }
             />
           ) : (
-            <View style={{ flex: 1, justifyContent: "center" }}>
-              <ActivityIndicator size="large" />
-            </View>
+            <LoadingScreen />
           )}
           <MovieSearchModal
             navigation={navigation}

f5aad80d1d7b67dae51d0c8612fb813c0336e593
cfoster5 <cfoster3204@gmail.com>
2022-06-13 19:40:53 -0500

    Update LoadingScreen.tsx

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, useWindowDimensions, View } from "react-native";
 
 export function LoadingScreen() {
+  const { width, height } = useWindowDimensions();
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
+    <View
+      style={{
+        position: "absolute",
+        top: height / 2 - 36 / 2,
+        left: width / 2 - 36 / 2,
+      }}
+    >
       <ActivityIndicator size="large" />
     </View>
   );

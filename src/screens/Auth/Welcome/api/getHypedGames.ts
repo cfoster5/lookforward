886706b82981e65735a9ba7133313bf5a0b392a3
cfoster5 <cfoster3204@gmail.com>
2022-09-12 12:48:19 -0500

    Update getHypedGames.ts

--- src/screens/Auth/Welcome/api/getHypedGames.ts
+++ src/screens/Auth/Welcome/api/getHypedGames.ts
@@ -1,14 +1,16 @@
 import { useQuery } from "react-query";
 
+import { igdbProxyKey } from "@/config";
+
 async function getGames() {
+  const unixTime = Math.floor(Date.now() / 1000);
   const response = await fetch(
-    "https://gou4rcsh6i.execute-api.us-east-1.amazonaws.com/prod/games",
+    "https://k0o7ncaic1.execute-api.us-east-2.amazonaws.com/production/v4/games",
     {
       method: "POST",
+      headers: { "x-api-key": igdbProxyKey },
       body: `fields name, category, hypes, first_release_date, cover.*, release_dates.*;
-    where category = 0 & first_release_date > ${Math.floor(
-      Date.now() / 1000
-    )} & release_dates.region = (2,8) & hypes != null & cover.url != null;
+    where category = 0 & first_release_date > ${unixTime} & release_dates.region = (2,8) & hypes != null & cover.url != null;
     sort hypes desc;
     limit 10;`,
     }

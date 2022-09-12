07fc1133e9b72c96811e2df6daf39d298d73e7c4
cfoster5 <cfoster3204@gmail.com>
2022-09-12 12:48:26 -0500

    Update getDiscoverGames.ts

--- src/screens/GameDiscover/api/getDiscoverGames.ts
+++ src/screens/GameDiscover/api/getDiscoverGames.ts
@@ -1,10 +1,13 @@
 import { useQuery } from "react-query";
 
+import { igdbProxyKey } from "@/config";
+
 async function getReleases(filter: string) {
   const response = await fetch(
-    "https://gou4rcsh6i.execute-api.us-east-1.amazonaws.com/prod/release_dates",
+    "https://k0o7ncaic1.execute-api.us-east-2.amazonaws.com/production/v4/release_dates",
     {
       method: "POST",
+      headers: { "x-api-key": igdbProxyKey },
       body: `fields *, game.name, game.summary, game.cover.*, game.genres.name, game.videos.name, game.videos.video_id, game.involved_companies.developer, game.involved_companies.porting, game.involved_companies.publisher, game.involved_companies.supporting, game.involved_companies.company.name, platform.abbreviation, platform.name;
     ${filter};
     limit 100;
@@ -22,11 +25,10 @@ export function useDiscoverGames({
   companyId?: number;
   keywordId?: number;
 }) {
-  let filter = `where date > ${Math.floor(Date.now() / 1000)} & region = (2,8)`;
+  const unixTime = Math.floor(Date.now() / 1000);
+  let filter = `where date > ${unixTime} & region = (2,8)`;
   filter += genreId ? ` & game.genres = (${genreId})` : ``;
   // filter += companyId ? `&with_companies=${companyId}` : ``;
   // filter += keywordId ? `&with_keywords=${keywordId}` : ``;
-  return useQuery(["discoverGames", { genreId: genreId }], () =>
-    getReleases(filter)
-  );
+  return useQuery(["discoverGames", { genreId }], () => getReleases(filter));
 }

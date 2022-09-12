fbdb6beca426a5b62cd7f2a6f322e8edc400d06a
cfoster5 <cfoster3204@gmail.com>
2022-09-12 12:48:34 -0500

    Update getGames.tsx

--- src/screens/Search/api/getGames.tsx
+++ src/screens/Search/api/getGames.tsx
@@ -1,27 +1,30 @@
-import { useQuery } from "react-query";
 import { convertReleasesToGames } from "helpers/helpers";
 import { IGDB } from "interfaces/igdb";
+import { useQuery } from "react-query";
+
+import { igdbProxyKey } from "@/config";
 
 async function getGames(searchValue: string) {
   if (!searchValue) {
+    const unixTime = Math.floor(Date.now() / 1000);
     const fields =
       "fields *, game.name, game.summary, game.cover.*, game.genres.name, game.videos.name, game.videos.video_id, game.involved_companies.developer, game.involved_companies.porting, game.involved_companies.publisher, game.involved_companies.supporting, game.involved_companies.company.name, platform.abbreviation, platform.name";
     const response = await fetch(
-      "https://gou4rcsh6i.execute-api.us-east-1.amazonaws.com/prod/release_dates",
+      "https://k0o7ncaic1.execute-api.us-east-2.amazonaws.com/production/v4/release_dates",
       {
         method: "POST",
-        body: `${fields}; where date > ${Math.floor(
-          Date.now() / 1000
-        )} & region = (2,8); limit 100; sort date;`,
+        headers: { "x-api-key": igdbProxyKey },
+        body: `${fields}; where date > ${unixTime} & region = (2,8); limit 100; sort date;`,
       }
     );
     const json: IGDB.ReleaseDate.ReleaseDate[] = await response.json();
     return convertReleasesToGames(json);
   } else {
     const response = await fetch(
-      "https://gou4rcsh6i.execute-api.us-east-1.amazonaws.com/prod/games",
+      "https://k0o7ncaic1.execute-api.us-east-2.amazonaws.com/production/v4/games",
       {
         method: "POST",
+        headers: { "x-api-key": igdbProxyKey },
         body: `fields name, summary, cover.*, release_dates.*, release_dates.platform.abbreviation, release_dates.platform.name, genres.name, videos.name, videos.video_id, involved_companies.developer, involved_companies.porting, involved_companies.publisher, involved_companies.supporting, involved_companies.company.name;
       where release_dates.region = (2,8);
       search "${searchValue}";
@@ -34,11 +37,7 @@ async function getGames(searchValue: string) {
 }
 
 export function useGames(searchValue: string) {
-  return useQuery(
-    ["games", { searchValue: searchValue }],
-    () => getGames(searchValue),
-    {
-      keepPreviousData: true,
-    }
-  );
+  return useQuery(["games", { searchValue }], () => getGames(searchValue), {
+    keepPreviousData: true,
+  });
 }

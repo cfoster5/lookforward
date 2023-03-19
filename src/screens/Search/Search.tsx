09c6657b9176343426650ebced95e50a6aeaa75a
cfoster5 <cfoster3204@gmail.com>
2023-03-19 17:02:03 -0500

    chore: Remove unused declarations

--- src/screens/Search/Search.tsx
+++ src/screens/Search/Search.tsx
@@ -1,72 +1,17 @@
-import {
-  BottomTabScreenProps,
-  useBottomTabBarHeight,
-} from "@react-navigation/bottom-tabs";
-import { CompositeScreenProps, useScrollToTop } from "@react-navigation/native";
+import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import { CompositeScreenProps } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import CategoryControl from "components/CategoryControl/CategoryControl";
-import { GamePlatformPicker } from "components/GamePlatformPicker";
-import { LoadingScreen } from "components/LoadingScreen";
-import { GamePoster } from "components/Posters/GamePoster";
-import { MoviePoster } from "components/Posters/MoviePoster";
 import { Text as ThemedText } from "components/Themed";
 import TabStackContext from "contexts/TabStackContext";
-import { Movie, Person, TMDB, TV } from "interfaces/tmdb";
-import { Search as SearchInterface } from "interfaces/tmdb/search";
-import { DateTime } from "luxon";
-import React, {
-  useContext,
-  useEffect,
-  useReducer,
-  useRef,
-  useState,
-} from "react";
-import {
-  FlatList,
-  Pressable,
-  SafeAreaView,
-  StyleSheet,
-  useWindowDimensions,
-  View,
-} from "react-native";
-import { KeyboardAwareFlatList } from "react-native-keyboard-aware-scroll-view";
-import { Modalize } from "react-native-modalize";
+import React, { useContext, useState } from "react";
+import { SafeAreaView } from "react-native";
 import { iOSUIKit } from "react-native-typography";
 
 import { GameLayout } from "./GameLayout";
 import { MovieLayout } from "./MovieLayout";
-import { useGames } from "./api/getGames";
-import { useMovieData } from "./api/getMovies";
-import MovieSearchModal from "./components/MovieSearchModal";
-import SearchPerson from "./components/SearchPerson";
-import useDebounce from "./hooks/useDebounce";
-import { MovieOption } from "./types";
 
-import { useStore } from "@/stores/store";
-import {
-  FindStackParamList,
-  Game,
-  ReleaseDate,
-  TabNavigationParamList,
-} from "@/types";
-
-function reducer(
-  state: any,
-  action: {
-    type: string;
-    categoryIndex?: number;
-  }
-) {
-  switch (action.type) {
-    case "set-categoryIndex":
-      return {
-        ...state,
-        categoryIndex: action.categoryIndex,
-      };
-    default:
-      return state;
-  }
-}
+import { FindStackParamList, TabNavigationParamList } from "@/types";
 
 export function ListLabel({ text, style }: { text: string; style?: any }) {
   return (
@@ -88,18 +33,8 @@ type FindScreenNavigationProp = CompositeScreenProps<
 >;
 
 function Search({ navigation, route }: FindScreenNavigationProp) {
-  const { width: windowWidth } = useWindowDimensions();
-  const [{ categoryIndex }, dispatch] = useReducer(reducer, {
-    categoryIndex: 0,
-  });
-
-  const scrollRef = useRef<FlatList>(null);
-  useScrollToTop(scrollRef);
+  const [categoryIndex, setCategoryIndex] = useState(0);
   const { theme } = useContext(TabStackContext);
-  const modalizeRef = useRef<Modalize>(null);
-  const { game } = useStore();
-  const tabBarheight = useBottomTabBarHeight();
-  const filterModalRef = useRef<Modalize>(null);
 
   // const scrollIndicatorInsets =
   //   Platform.OS === "ios" ? { bottom: tabBarheight - 16 } : undefined;
@@ -112,9 +47,7 @@ function Search({ navigation, route }: FindScreenNavigationProp) {
         <CategoryControl
           buttons={["Movies", "Games"]}
           categoryIndex={categoryIndex}
-          handleCategoryChange={(index) =>
-            dispatch({ type: "set-categoryIndex", categoryIndex: index })
-          }
+          handleCategoryChange={(index) => setCategoryIndex(index)}
         />
       </SafeAreaView>
       {categoryIndex === 0 ? (
@@ -125,16 +58,5 @@ function Search({ navigation, route }: FindScreenNavigationProp) {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  flatlistContentContainer: {
-    marginHorizontal: 16,
-    // paddingBottom: Platform.OS === "ios" ? tabBarheight : undefined,
-  },
-  flatlistColumnWrapper: {
-    justifyContent: "space-between",
-    marginBottom: 16,
-  },
-});
 
 export default Search;

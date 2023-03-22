26d020b57d63c8570d9c324b2137c30a1632c50c
cfoster5 <cfoster3204@gmail.com>
2023-03-22 13:54:57 -0500

    refactor: Create DynamicHeightModal wrapper

---
+++ src/components/DynamicHeightModal.tsx
@@ -0,0 +1,46 @@
+import {
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
+  BottomSheetModal,
+  BottomSheetModalProps,
+  useBottomSheetDynamicSnapPoints,
+} from "@gorhom/bottom-sheet";
+import React, { cloneElement, useMemo } from "react";
+import { PlatformColor } from "react-native";
+
+const RenderBackdrop = (props: BottomSheetBackdropProps) => (
+  <BottomSheetBackdrop {...props} appearsOnIndex={0} disappearsOnIndex={-1} />
+);
+
+export function DynamicHeightModal({
+  modalRef,
+  children,
+}: {
+  modalRef;
+  children: BottomSheetModalProps["children"];
+}) {
+  const snapPoints = useMemo(() => ["CONTENT_HEIGHT"], []);
+  const {
+    animatedHandleHeight,
+    animatedSnapPoints,
+    animatedContentHeight,
+    handleContentLayout,
+  } = useBottomSheetDynamicSnapPoints(snapPoints);
+
+  return (
+    <BottomSheetModal
+      ref={modalRef}
+      snapPoints={animatedSnapPoints}
+      handleHeight={animatedHandleHeight}
+      contentHeight={animatedContentHeight}
+      backdropComponent={RenderBackdrop}
+      backgroundStyle={{
+        backgroundColor: PlatformColor("secondarySystemBackground"),
+      }}
+      handleIndicatorStyle={{
+        backgroundColor: PlatformColor("systemGray"),
+      }}
+      children={cloneElement(children, { onLayout: handleContentLayout })}
+    />
+  );
+}

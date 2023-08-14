a7361931cdbbf9810e8b3170dbedeafe29ce9924
cfoster5 <cfoster3204@gmail.com>
2023-08-14 07:58:44 -0500

    chore: Get Trakt and OMDB keys from env variables

--- app.config.js
+++ app.config.js
@@ -46,6 +46,8 @@ export default {
       },
       TMDB_KEY: process.env.TMDB_KEY || null,
       IGDB_AWS_KEY: process.env.IGDB_AWS_KEY || null,
+      TRAKT_KEY: process.env.TRAKT_KEY || null,
+      OMDB_KEY: process.env.OMDB_KEY || null,
     },
   },
 };

694b0bd8417c0087b2f53e09ff18b99b04126a36
cfoster5 <cfoster3204@gmail.com>
2024-10-25 20:27:01 -0500

    refactor: Split Store type into State and Actions types

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -10,32 +10,30 @@ const bottomSheetModalRef = createRef<BottomSheetModal>();
 const onboardingModalRef = createRef<BottomSheetModal>();
 const proModalRef = createRef<BottomSheetModal>();
 
-export enum Subs {
-  movieSubs = "movieSubs",
-  gameSubs = "gameSubs",
-}
-
-type Store = {
+type State = {
   user: FirebaseAuthTypes.User | null;
-  setUser: (user: FirebaseAuthTypes.User | null) => void;
   theme: ColorSchemeName;
-  setTheme: (theme: ColorSchemeName) => void;
   movieSubs: FirestoreMovie[];
-  setMovieSubs: (movies: FirestoreMovie[]) => void;
-  gameSubs: any;
-  setGameSubs: (games: any) => void;
+  gameSubs: FirestoreGame[];
   categoryIndex: number;
-  setCategoryIndex: (number: Store["categoryIndex"]) => void;
   bottomSheetModalRef: typeof bottomSheetModalRef;
   onboardingModalRef: typeof onboardingModalRef;
   proModalRef: typeof proModalRef;
   isPro: boolean;
-  setIsPro: (isPro: Store["isPro"]) => void;
   initialSnapPoint: number;
+};
+
+type Actions = {
+  setUser: (user: FirebaseAuthTypes.User | null) => void;
+  setTheme: (theme: ColorSchemeName) => void;
+  setMovieSubs: (movies: FirestoreMovie[]) => void;
+  setGameSubs: (games: FirestoreGame[]) => void;
+  setCategoryIndex: (number: number) => void;
+  setIsPro: (isPro: boolean) => void;
   setInitialSnapPoint: (value: number) => void;
 };
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create<State & Actions>((set) => ({
   user: null,
   setUser: (user) => set(() => ({ user })),
   theme: "dark",

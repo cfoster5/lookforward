a06ebe468d6faabca5e403023f8747258d84e1ca
cfoster5 <cfoster3204@gmail.com>
2024-10-25 21:17:02 -0500

    refactor: Update names for some getMovieCountdowns variables

--- src/screens/Countdown/api/getMovieCountdowns.ts
+++ src/screens/Countdown/api/getMovieCountdowns.ts
@@ -3,30 +3,33 @@ import { ReleaseDateType } from "tmdb-ts";
 
 import { FirestoreMovie } from "@/interfaces/firebase";
 import { tmdb } from "@/providers/app";
-import { isoToUTC } from "@/utils/dates";
+import { compareDates, isoToUTC } from "@/utils/dates";
 import { useStore } from "@/stores/store";
 
 async function getMovie(movieId: FirestoreMovie["documentID"]) {
   const json = await tmdb.movies.details(
     Number(movieId),
     ["release_dates"],
-    "en-US"
+    "en-US",
   );
 
   const usRelease = json.release_dates.results.find(
-    (result) => result.iso_3166_1 === "US"
+    (result) => result.iso_3166_1 === "US",
   );
-  const filteredReleases = usRelease?.release_dates.filter(
-    (release) => release.type !== ReleaseDateType.Premiere
+
+  const nonPremiereDates = usRelease?.release_dates.filter(
+    (release) => release.type !== ReleaseDateType.Premiere,
   );
-  const sortedReleases = filteredReleases?.sort(
-    (a, b) => isoToUTC(a.release_date) > isoToUTC(b.release_date)
+
+  const sortedNonPremiereDates = nonPremiereDates?.sort(
+    ({ release_date: a }, { release_date: b }) =>
+      compareDates(isoToUTC(a), isoToUTC(b)),
   );
-  const date = sortedReleases?.[0]?.release_date;
+
+  const date = sortedNonPremiereDates?.[0]?.release_date;
 
   return {
     ...json,
-    // traktReleaseDate: traktJson.released,
     releaseDate: date,
     documentID: movieId,
   };

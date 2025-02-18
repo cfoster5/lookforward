244d7dd6fb9e05e5348674796acfae6cc1297ec3
cfoster5 <cfoster3204@gmail.com>
2025-02-18 15:58:35 -0600

    refactor: Update TabStack to use new naming conventions for tab screens

--- src/navigation/TabStack.tsx
+++ src/navigation/TabStack.tsx
@@ -3,17 +3,17 @@ import { BlurView } from "@react-native-community/blur";
 import firestore from "@react-native-firebase/firestore";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useEffect } from "react";
-import { Platform, PlatformColor, StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
 import { CountdownStack } from "./CountdownStack";
 import { FindStack } from "./FindStack";
 import { SettingsStack } from "./SettingsStack";
-import { FirestoreGame, FirestoreMovie } from "../interfaces/firebase";
+import { FirestoreMovie } from "../interfaces/firebase";
 
 import { useStore } from "@/stores/store";
-import { BottomTabParams } from "@/types";
+import { TabNavigationParamList } from "@/types";
 
-const Tab = createBottomTabNavigator<BottomTabParams>();
+const Tab = createBottomTabNavigator<TabNavigationParamList>();
 export function TabStack() {
   const { user, setMovieSubs, setGameSubs } = useStore();
 
@@ -27,7 +27,7 @@ export function TabStack() {
             ...doc.data(),
             subscribers: doc.data()["subscribers"],
             documentID: doc.id,
-          })
+          }),
         );
         setMovieSubs(movieSubsData);
       });
@@ -56,11 +56,11 @@ export function TabStack() {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           let iconName = "";
-          if (route.name === "FindTabStack") {
+          if (route.name === "FindTab") {
             iconName = "search";
-          } else if (route.name === "CountdownTabStack") {
+          } else if (route.name === "CountdownTab") {
             iconName = "timer-outline";
-          } else if (route.name === "SettingsTabStack") {
+          } else if (route.name === "SettingsTab") {
             iconName = "cog";
           }
           return <Ionicons name={iconName} size={size} color={color} />;
@@ -79,17 +79,17 @@ export function TabStack() {
     >
       {/* Is setting headerShown to false the best method? */}
       <Tab.Screen
-        name="FindTabStack"
+        name="FindTab"
         component={FindStack}
         options={{ headerShown: false, tabBarLabel: "Find" }}
       />
       <Tab.Screen
-        name="CountdownTabStack"
+        name="CountdownTab"
         component={CountdownStack}
         options={{ headerShown: false, tabBarLabel: "Countdown" }}
       />
       <Tab.Screen
-        name="SettingsTabStack"
+        name="SettingsTab"
         component={SettingsStack}
         options={{ headerShown: false, tabBarLabel: "Settings" }}
       />

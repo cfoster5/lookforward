2926c1e07ccc2c8031fc0b8553b99308c941a9f3
cfoster5 <cfoster3204@gmail.com>
2025-02-18 16:02:22 -0600

    refactor: Update isoToUTC function to accept Date type instead of string

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -7,7 +7,7 @@ export const timestamp = now.toSeconds();
 export const timestampToUTC = (timestamp: number) =>
   DateTime.fromSeconds(timestamp).toUTC();
 
-export const isoToUTC = (iso: string) => DateTime.fromISO(iso).toUTC();
+export const isoToUTC = (iso: Date) => DateTime.fromISO(iso).toUTC();
 
 export const compareDates = (a: DateTime, b: DateTime) =>
   a.toMillis() - b.toMillis();

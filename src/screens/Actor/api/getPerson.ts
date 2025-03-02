1a8ea82cbd1260323f1559342a5308a56f510bb7
cfoster5 <cfoster3204@gmail.com>
2025-03-02 15:40:15 -0600

    refactor: Update getPerson and usePerson to use typed parameters from tmdb API

--- src/screens/Actor/api/getPerson.ts
+++ src/screens/Actor/api/getPerson.ts
@@ -1,11 +1,12 @@
+import { tmdb } from "@/providers/app";
 import { useQuery } from "@tanstack/react-query";
 
-import { tmdb } from "@/providers/app";
-
-const getPerson = async (personId: number) =>
+const getPerson = async (personId: Parameters<typeof tmdb.people.details>[0]) =>
   await tmdb.people.details(personId, ["movie_credits", "images"], "en-US");
 
-export const usePerson = (personId: number) =>
+export const usePerson = (
+  personId: Parameters<typeof tmdb.people.details>[0],
+) =>
   useQuery({
     queryKey: ["person", personId],
     queryFn: () => getPerson(personId),

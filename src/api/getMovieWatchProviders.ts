9f78e36544d551e02c3efa88339600e212067c63
cfoster5 <cfoster3204@gmail.com>
2025-03-10 20:15:35 -0500

    refactor: Replace fetch call with tmdb provider for movie watch providers

--- src/api/getMovieWatchProviders.ts
+++ src/api/getMovieWatchProviders.ts
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 
+import { tmdb } from "@/providers/app";
+
 async function getProviders() {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/watch/providers/movie?api_key=68991fbb0b75dba5ae0ecd8182e967b1&language=en-US&watch_region=US`,
-  );
-  const json = await response.json();
-  return json;
+  return await tmdb.watchProviders.getMovieProviders({
+    language: "en-US",
+    watch_region: "US",
+  });
 }
 
 export function useMovieWatchProviders() {

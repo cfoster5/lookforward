4767c0005da824f2dc43a015f01074c58b66516a
cfoster5 <cfoster3204@gmail.com>
2025-03-16 21:00:07 -0500

    chore: Add import/order rule to ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,21 @@ module.exports = {
     "react/no-unstable-nested-components": ["warn"],
     "max-params": ["warn", 3],
     "import/no-cycle": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+        ],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
   },
 };

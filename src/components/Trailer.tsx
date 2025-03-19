6b9c743bb5ba12a42be1de82fababd9dbbd99d9e
cfoster5 <cfoster3204@gmail.com>
2025-03-18 19:43:43 -0500

    refactor: Simplify video handling in Trailer component by unifying video type and URL generation

--- src/components/Trailer.tsx
+++ src/components/Trailer.tsx
@@ -1,21 +1,21 @@
 import { Image } from "expo-image";
 import { Linking, PlatformColor, Pressable, Text } from "react-native";
 import { iOSUIKit } from "react-native-typography";
-import { Video } from "tmdb-ts";
+import { Video as MovieVideo } from "tmdb-ts";
 
-import { calculateWidth } from "../helpers/helpers";
-
+import { calculateWidth } from "@/helpers/helpers";
 import { Video as GameVideo } from "@/types";
 
-function Trailer({ video }: { video: Video | GameVideo }) {
+type TrailerProps = {
+  video: MovieVideo | GameVideo;
+};
+
+function Trailer({ video }: TrailerProps) {
+  const videoId = (video as MovieVideo).key || (video as GameVideo).video_id;
   return (
     <Pressable
       onPress={() =>
-        Linking.openURL(
-          (video as Video).key
-            ? `https://www.youtube.com/watch?v=${(video as Video).key}`
-            : `https://www.youtube.com/watch?v=${(video as GameVideo).video_id}`
-        )
+        Linking.openURL(`https://www.youtube.com/watch?v=${videoId}`)
       }
       style={{ width: calculateWidth(16, 8, 1.5) }}
     >
@@ -26,22 +26,13 @@ function Trailer({ video }: { video: Video | GameVideo }) {
           borderWidth: 1,
           borderColor: PlatformColor("separator"),
         }}
-        source={{
-          uri: (video as Video).key
-            ? `https://img.youtube.com/vi/${(video as Video).key}/hqdefault.jpg`
-            : `https://img.youtube.com/vi/${
-                (video as GameVideo).video_id
-              }/mqdefault.jpg`,
-        }}
+        source={{ uri: `https://img.youtube.com/vi/${videoId}/hqdefault.jpg` }}
         contentFit="cover"
       />
       <Text
         style={[
-          iOSUIKit.subheadEmphasized,
-          {
-            color: PlatformColor("secondaryLabel"),
-            marginTop: 8,
-          },
+          iOSUIKit.subhead,
+          { color: PlatformColor("label"), marginTop: 8 },
         ]}
       >
         {video.name}

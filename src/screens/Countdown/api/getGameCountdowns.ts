2375dd37c6bcb9fd8b1ed8a572a2e77d30823db8
cfoster5 <cfoster3204@gmail.com>
2025-04-10 19:52:10 -0500

    refactor: Return complete response data in getGameRelease and adjust selection in useGameCountdowns

--- src/screens/Countdown/api/getGameCountdowns.ts
+++ src/screens/Countdown/api/getGameCountdowns.ts
@@ -7,7 +7,7 @@ import { ReleaseDate } from "@/types/igdb";
 export async function getGameRelease(releaseId: ReleaseDate["id"]) {
   const query = `fields human, date, game.name, game.cover.*; where id = ${releaseId};`;
   const response = await igdb.releaseDates.retreiveReleaseDate(query);
-  return response.data[0];
+  return response.data;
 }
 
 export function useGameCountdowns() {
@@ -16,6 +16,7 @@ export function useGameCountdowns() {
     queries: gameSubs.map((sub) => ({
       queryKey: ["gameRelease", Number(sub.documentID)],
       queryFn: () => getGameRelease(Number(sub.documentID)),
+      select: (releaseDates) => releaseDates[0],
     })),
   });
 }

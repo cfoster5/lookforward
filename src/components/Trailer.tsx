13d482e15a147b9209c28573c404f1e423476e73
cfoster5 <cfoster3204@gmail.com>
2025-04-20 17:39:53 -0500

    feat: Add YouTube player directly within app for trailers

--- src/components/Trailer.tsx
+++ src/components/Trailer.tsx
@@ -1,44 +1,105 @@
 import { Image } from "expo-image";
-import { Linking, PlatformColor, Pressable, Text } from "react-native";
+import { useState } from "react";
+import {
+  PlatformColor,
+  Pressable,
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  Modal,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { iOSUIKit } from "react-native-typography";
+import YoutubePlayer from "react-native-youtube-iframe";
 import { Video as MovieVideo } from "tmdb-ts";
 
 import { calculateWidth } from "@/helpers/helpers";
-import { Video as GameVideo } from "@/types";
+import { GameVideo } from "@/types/igdb";
 
 type TrailerProps = {
   video: MovieVideo | GameVideo;
 };
 
 function Trailer({ video }: TrailerProps) {
+  const [modalVisible, setModalVisible] = useState(false);
+  // calculate player dimensions
+  const screenWidth = Dimensions.get("window").width;
+  const playerHeight = (screenWidth / 16) * 9;
+  const playing = modalVisible;
   const videoId = (video as MovieVideo).key || (video as GameVideo).video_id;
+  const { top: safeTopArea, right: safeRightArea } = useSafeAreaInsets();
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "black",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    closeButton: {
+      position: "absolute",
+      top: safeTopArea,
+      right: safeRightArea,
+      padding: 16,
+    },
+  });
+
   return (
-    <Pressable
-      onPress={() =>
-        Linking.openURL(`https://www.youtube.com/watch?v=${videoId}`)
-      }
-      style={{ width: calculateWidth(16, 8, 1.5) }}
-    >
-      <Image
-        style={{
-          aspectRatio: 16 / 9,
-          borderRadius: 12,
-          borderWidth: 1,
-          borderColor: PlatformColor("separator"),
-        }}
-        source={{ uri: `https://img.youtube.com/vi/${videoId}/hqdefault.jpg` }}
-        contentFit="cover"
-      />
-      <Text
-        numberOfLines={2}
-        style={[
-          iOSUIKit.subhead,
-          { color: PlatformColor("label"), marginTop: 8 },
-        ]}
+    <>
+      <Pressable
+        onPress={() => setModalVisible(true)}
+        style={{ width: calculateWidth(16, 8, 1.5) }}
       >
-        {video.name}
-      </Text>
-    </Pressable>
+        <Image
+          style={{
+            aspectRatio: 16 / 9,
+            borderRadius: 12,
+            borderWidth: 1,
+            borderColor: PlatformColor("separator"),
+          }}
+          source={{
+            uri: `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`,
+          }}
+          contentFit="cover"
+        />
+        <Text
+          numberOfLines={2}
+          style={[
+            iOSUIKit.subhead,
+            { color: PlatformColor("label"), marginTop: 8 },
+          ]}
+        >
+          {video.name}
+        </Text>
+      </Pressable>
+      <Modal
+        visible={modalVisible}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.container}>
+          <YoutubePlayer
+            height={playerHeight}
+            width={screenWidth}
+            play={playing}
+            videoId={videoId}
+            onChangeState={(state) => {
+              if (state === "ended") setModalVisible(false);
+            }}
+          />
+          <Pressable
+            style={styles.closeButton}
+            accessibilityLabel="Close video"
+            onPress={() => setModalVisible(false)}
+          >
+            <Text style={[iOSUIKit.body, { color: PlatformColor("label") }]}>
+              Close
+            </Text>
+          </Pressable>
+        </View>
+      </Modal>
+    </>
   );
 }
 

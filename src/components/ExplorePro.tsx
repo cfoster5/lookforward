3bae1f5e412236ce35be63f89f2a949d4bf9cccf
cfoster5 <cfoster3204@gmail.com>
2025-11-08 21:10:44 -0600

    refactor: update handling of errors and purchases attempts/restoration

--- src/components/ExplorePro.tsx
+++ src/components/ExplorePro.tsx
@@ -17,9 +17,31 @@ import { iOSUIKit } from "react-native-typography";
 import { DynamicHeightModal } from "@/components/DynamicHeightModal";
 import { Row } from "@/components/Row";
 import { useAuthStore, useInterfaceStore } from "@/stores";
+import { tryCatch } from "@/utils/try-catch";
 
 import { LargeFilledButton } from "./LargeFilledButton";
 import { SubscriptionOption } from "./SubscriptionOption";
+
+type FooterButtonProps = {
+  url: string;
+  text: string;
+};
+
+const FooterButton = ({ url, text }: FooterButtonProps) => (
+  <Pressable
+    style={{
+      minHeight: 28,
+      minWidth: 28,
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+    onPress={() => Linking.openURL(url)}
+  >
+    <Text style={[iOSUIKit.footnote, { color: Colors.systemBlue }]}>
+      {text}
+    </Text>
+  </Pressable>
+);
 
 export const ExplorePro = () => {
   const { bottom: safeBottomArea } = useSafeAreaInsets();
@@ -28,8 +50,8 @@ export const ExplorePro = () => {
   const [selectedProduct, setSelectedProduct] = useState<PurchasesPackage>();
   const [isPurchasing, setIsPurchasing] = useState(false);
 
-  const { data: purchaseOfferings, error } = useQuery({
-    queryKey: ["explore-pro"],
+  const { data: purchaseOfferings } = useQuery({
+    queryKey: ["purcaseOfferings"],
     queryFn: async () => await Purchases.getOfferings(),
   });
 
@@ -40,44 +62,43 @@ export const ExplorePro = () => {
   }, [purchaseOfferings]);
 
   async function handlePurchase() {
+    if (!selectedProduct) return;
     setIsPurchasing(true);
-    // Using Offerings/Packages
-    try {
-      if (!purchaseOfferings?.all["pro"]?.lifetime) {
-        console.error("No lifetime package available");
-        return;
-      }
-      const { customerInfo } = await Purchases.purchasePackage(
-        selectedProduct!,
-        // products.lifetime,
-      );
-      if (typeof customerInfo.entitlements.active.pro !== "undefined") {
-        // Unlock that great "pro" content
-        setIsPro(true);
-        Alert.alert(
-          "Thank you!",
-          "Thank you so much for your support. Please enjoy your Pro content.",
-        );
-      }
-    } catch (error) {
-      if (!error.userCancelled) {
-        Alert.alert("Error purchasing package", e.message);
-      }
-    } finally {
+    const { data, error } = await tryCatch(
+      Purchases.purchasePackage(selectedProduct),
+    );
+
+    if (error) {
       setIsPurchasing(false);
+      if (!error.userCancelled) {
+        // Only show alert if not cancelled by user
+        Alert.alert(error.message);
+      }
+      return;
     }
+
+    const { customerInfo } = data;
+    if (typeof customerInfo.entitlements.active["pro"] !== "undefined") {
+      setIsPro(true);
+      Alert.alert(
+        "Thank you!",
+        "Thank you so much for your support. Please enjoy your Pro content.",
+      );
+    }
+    setIsPurchasing(false); // Add here at the end
   }
 
   async function handleRestorePurchase() {
-    try {
-      const restore = await Purchases.restorePurchases();
-      // ... check restored purchaserInfo to see if entitlement is now active
-      if (restore.entitlements.active.pro) {
-        setIsPro(true);
-        Alert.alert("Purchase restored");
-      }
-    } catch (error) {
-      console.error(error);
+    const { data: customerInfo, error } = await tryCatch(
+      Purchases.restorePurchases(),
+    );
+    if (error) {
+      Alert.alert("Error restoring purchases", error.message);
+      return;
+    }
+    if (customerInfo.entitlements.active["pro"]) {
+      setIsPro(true);
+      Alert.alert("Purchase restored");
     }
   }
 
@@ -105,26 +126,6 @@ export const ExplorePro = () => {
         >
           LookForward Pro
         </Text>
-        {/* <Text
-          style={[
-            iOSUIKit.title3Emphasized,
-            { color: Colors.systemBlue, textAlign: "center" },
-          ]}
-        >
-          {products?.lifetime?.product.priceString}
-        </Text>
-        <Text
-          style={[
-            iOSUIKit.title3,
-            {
-              color: Colors.label,
-              textAlign: "center",
-              paddingBottom: 16,
-            },
-          ]}
-        >
-          Unlock once. Enjoy forever.
-        </Text> */}
         <Row
           icon="list.dash"
           title="Unlimited Countdown Items"
@@ -149,55 +150,60 @@ export const ExplorePro = () => {
           body="View ticket sales at the theaters."
           useAltIcon
         />
-        <SubscriptionOption
-          handlePress={() => {
-            if (purchaseOfferings?.all["pro"].monthly) {
-              setSelectedProduct(purchaseOfferings?.all["pro"].monthly);
+        {purchaseOfferings?.all["pro"]?.monthly && (
+          <SubscriptionOption
+            handlePress={() => {
+              if (purchaseOfferings.all["pro"].monthly) {
+                setSelectedProduct(purchaseOfferings.all["pro"].monthly);
+              }
+            }}
+            text={`${purchaseOfferings.all["pro"].monthly.product.priceString} Monthly`}
+            isSelected={
+              purchaseOfferings.all["pro"].monthly.identifier ===
+              selectedProduct?.identifier
             }
-          }}
-          text={`${purchaseOfferings?.all["pro"].monthly?.product.priceString} Monthly`}
-          isSelected={
-            purchaseOfferings?.all["pro"].monthly?.identifier ===
-            selectedProduct?.identifier
-          }
-          style={{ marginTop: 16, marginBottom: 8 }}
-        />
-        <SubscriptionOption
-          handlePress={() => {
-            if (purchaseOfferings?.all["pro"].annual) {
-              setSelectedProduct(purchaseOfferings?.all["pro"].annual);
+            style={{ marginTop: 16, marginBottom: 8 }}
+          />
+        )}
+        {purchaseOfferings?.all["pro"]?.annual && (
+          <SubscriptionOption
+            handlePress={() => {
+              if (purchaseOfferings.all["pro"].annual) {
+                setSelectedProduct(purchaseOfferings.all["pro"].annual);
+              }
+            }}
+            text={`${purchaseOfferings.all["pro"].annual.product.priceString} Yearly`}
+            isSelected={
+              purchaseOfferings.all["pro"].annual.identifier ===
+              selectedProduct?.identifier
             }
-          }}
-          text={`${purchaseOfferings?.all["pro"]?.annual?.product.priceString} Yearly`}
-          isSelected={
-            purchaseOfferings?.all["pro"]?.annual?.identifier ===
-            selectedProduct?.identifier
-          }
-          style={{ marginTop: 8, marginBottom: 16 }}
-        />
-        <LargeFilledButton
-          disabled={false}
-          style={{ marginVertical: 16 }}
-          handlePress={() => handlePurchase()}
-        >
-          {isPurchasing ? (
-            <ActivityIndicator
-              color="white"
-              // Match size to below text's lineHeight so pressable height isn't changed
-              size={iOSUIKit.bodyEmphasizedObject.lineHeight}
-            />
-          ) : (
-            <Text
-              style={[
-                iOSUIKit.bodyEmphasized,
-                { color: "white", textAlign: "center" },
-              ]}
-            >
-              {/* Unlock forever for {products?.lifetime?.product.priceString} */}
-              Unlock for {selectedProduct?.product.priceString}
-            </Text>
-          )}
-        </LargeFilledButton>
+            style={{ marginTop: 8, marginBottom: 16 }}
+          />
+        )}
+        {selectedProduct && (
+          <LargeFilledButton
+            disabled={false}
+            style={{ marginVertical: 16 }}
+            handlePress={() => handlePurchase()}
+          >
+            {isPurchasing ? (
+              <ActivityIndicator
+                color="white"
+                // Match size to below text's lineHeight so pressable height isn't changed
+                size={iOSUIKit.bodyEmphasizedObject.lineHeight}
+              />
+            ) : (
+              <Text
+                style={[
+                  iOSUIKit.bodyEmphasized,
+                  { color: "white", textAlign: "center" },
+                ]}
+              >
+                Unlock for {selectedProduct.product.priceString}
+              </Text>
+            )}
+          </LargeFilledButton>
+        )}
         <Pressable
           style={{
             minHeight: 44,
@@ -218,38 +224,14 @@ export const ExplorePro = () => {
             marginTop: 16,
           }}
         >
-          <Pressable
-            style={{
-              minHeight: 28,
-              minWidth: 28,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-            onPress={() =>
-              Linking.openURL(
-                "https://www.apple.com/legal/internet-services/itunes/dev/stdeula",
-              )
-            }
-          >
-            <Text style={[iOSUIKit.footnote, { color: Colors.systemBlue }]}>
-              Terms of Service
-            </Text>
-          </Pressable>
-          <Pressable
-            style={{
-              minHeight: 28,
-              minWidth: 28,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-            onPress={() =>
-              Linking.openURL("https://getlookforward.app/privacy")
-            }
-          >
-            <Text style={[iOSUIKit.footnote, { color: Colors.systemBlue }]}>
-              Privacy Policy
-            </Text>
-          </Pressable>
+          <FooterButton
+            url="https://www.apple.com/legal/internet-services/itunes/dev/stdeula"
+            text="Terms of Service"
+          />
+          <FooterButton
+            url="https://getlookforward.app/privacy"
+            text="Privacy Policy"
+          />
         </View>
       </BottomSheetView>
     </DynamicHeightModal>
